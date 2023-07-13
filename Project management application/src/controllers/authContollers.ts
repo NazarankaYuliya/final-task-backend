08237fc10cfa4042fe92c4b279151d52a5413fbb
juliya <juliya@MacBook-Air-juliya.local>
2023-07-13 13:17:26 +0200

    feat: add mongodb link

--- Project management application/src/controllers/authContollers.ts
+++ Project management application/src/controllers/authContollers.ts
@@ -5,32 +5,31 @@ import { checkPassword, hashPassword } from '../services/hash.service';
 import { signToken } from '../services/token.service';
 
 export const signIn = async (req: Request, res: Response) => {
-
-  const bodyError = checkBody(req.body, ['login', 'password'])
+  const bodyError = checkBody(req.body, ['login', 'password']);
   if (bodyError) {
-    return res.status(400).send(createError(400, "bad request: " + bodyError));
+    return res.status(400).send(createError(400, 'bad request: ' + bodyError));
   }
 
   const { login, password } = req.body;
 
   const foundedUser = await userService.findOneUser({ login });
   if (foundedUser) {
-    const isCorrectPassword = await checkPassword(password, foundedUser.password);
+    const isCorrectPassword = await checkPassword(
+      password,
+      foundedUser.password,
+    );
     if (isCorrectPassword) {
-      return res.send({ token: signToken(foundedUser._id, login) })
+      return res.send({ token: signToken(foundedUser._id, login) });
     }
   }
 
   return res.status(401).send(createError(401, 'Authorization error'));
-
 };
 
-
 export const signUp = async (req: Request, res: Response) => {
-
-  const bodyError = checkBody(req.body, ['name', 'login', 'password'])
+  const bodyError = checkBody(req.body, ['name', 'login', 'password']);
   if (bodyError) {
-    return res.status(400).send(createError(400, "bad request: " + bodyError));
+    return res.status(400).send(createError(400, 'bad request: ' + bodyError));
   }
   const { login, name, password } = req.body;
 
@@ -41,10 +40,15 @@ export const signUp = async (req: Request, res: Response) => {
   const hashedPassword = await hashPassword(password);
 
   try {
-    const newUser = await userService.createUser({ login, name, password: hashedPassword });
+    const newUser = await userService.createUser({
+      login,
+      name,
+      password: hashedPassword,
+    });
     res.json(newUser);
+  } catch (err) {
+    return console.log(err);
   }
-  catch (err) { return console.log(err); }
 };
 
 export const chek = async (req: Request, res: Response) => {
--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,19 +3,18 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
-
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      'mongodb+srv://admin:admin1234@cluster0.dqwhsjx.mongodb.net/?retryWrites=true&w=majority',
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
-    })
+    });
   } catch (error) {
     console.log(error);
   }
 })();
-
-
 
 process.on('SIGINT', async () => {
   await mongoose.disconnect();
